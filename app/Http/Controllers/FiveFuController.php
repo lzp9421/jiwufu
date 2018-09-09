eee1041d327d52f20a08859cb89f78d1e477152d
lizhipeng <mail@lzp.name>
2018-09-09 16:56:59 +0800

    用户卡片列表

--- app/Http/Controllers/FiveFuController.php
+++ app/Http/Controllers/FiveFuController.php
@@ -12,7 +12,42 @@ class FiveFuController extends Controller
 {
     public function index()
     {
-        return view('fivefu');
+        $wechat_user = session('wechat.oauth_user'); // 拿到授权用户资料
+        $wechat_user = reset($wechat_user)->toArray();
+        $data = [];
+        $data['wechat_id'] = $wechat_user['id'];
+        $data['name'] = $wechat_user['name'];
+        $data['nickname'] = $wechat_user['nickname'] ?? '';
+        $data['avatar'] = $wechat_user['avatar'] ?? '';
+        $data['email'] = $wechat_user['email'] ?? '';
+
+        $data = User::updateOrCreate($data)->toArray();
+
+        // 剩余抽奖次数
+        $max_lottery = 10;
+
+        $lottery_num = UserLottery::countByUserIdDay($data['id'], date('Ymd'));
+        $data['residue'] = $max_lottery - $lottery_num;
+
+        // 用户卡片列表
+        $user_cards = UserCard::findByUserId($data['id'])->toArray();
+        foreach ($user_cards as $key => $user_card) {
+            unset($user_cards[$key]);
+            $card_id = $user_card['card_id'];
+            $user_cards[$card_id] = $user_card;
+        }
+        $cards = Card::all()->toArray();
+        foreach ($cards as &$card) {
+            if (empty($user_cards[$card['id']])) {
+                $card['num'] = 0;
+                continue;
+            }
+            $user_card = $user_cards[$card['id']];
+            $card['num'] = $user_card['num'];
+        }
+
+        $data['cards'] = $cards;
+        return view('fivefu', compact('data'));
     }
 
     /**
