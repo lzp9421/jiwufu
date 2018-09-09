426a497eda20be28d43b7ecb682c0ef4252dfe39
lizhipeng <mail@lzp.name>
2018-09-09 17:44:08 +0800

    用户卡片列表

--- database/migrations/2018_09_09_020722_create_card_table.php
+++ database/migrations/2018_09_09_020722_create_card_table.php
@@ -16,8 +16,11 @@ class CreateCardTable extends Migration
         Schema::create('card', function (Blueprint $table) {
             $this->engine = 'Innodb';
             $table->increments('id');
-            $table->string('name', 50)->default('')->comment('卡片名');
+            $table->string('name', 50)->default('')->comment('卡片标识');
+            $table->string('title', 50)->default('')->comment('卡片名');
             $table->string('description', 255)->default('')->comment('卡片说明');
+            $table->string('image', 255)->default('')->comment('卡片图片');
+            $table->string('thumb', 255)->default('')->comment('卡片小图');
             $table->unsignedInteger('create_time')->default(0);
             $table->unsignedInteger('update_time')->default(0);
         });
