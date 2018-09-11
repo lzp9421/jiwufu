28198cd379b8f938ccf88da4afe3a0fde2de17d4
lizhipeng <mail@lzp.name>
2018-09-11 21:12:23 +0800

    微信分享配置

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -30,3 +30,27 @@ function verify_data($token, ...$vars) {
     $cale_token = md5(serialize($vars) . SECRET);
     return $token === $cale_token;
 }
+
+/**
+ * @return string
+ */
+function current_url()
+{
+    $pageURL = 'http';
+
+    if ($_SERVER["HTTPS"] == "on")
+    {
+        $pageURL .= "s";
+    }
+    $pageURL .= "://";
+
+    if ($_SERVER["SERVER_PORT"] != "80")
+    {
+        $pageURL .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
+    }
+    else
+    {
+        $pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+    }
+    return $pageURL;
+}
